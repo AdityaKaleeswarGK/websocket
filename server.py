dfe963df645e7569548993b10dbff71f9b5d11ec
AdityaKaleeswarGK <145224338+AdityaKaleeswarGK@users.noreply.github.com>
2024-06-28 23:36:09 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,56 @@
+import socket
+import select
+
+headsize = 10
+ip = "127.0.0.1"
+port = 1234
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((ip, port))
+s.listen()
+
+sockets_list = [s]
+clients = {}
+
+print(f'Listening for connections on {ip}:{port}...')
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(headsize)
+        if not len(message_header):
+            return False
+        message_length = int(message_header.decode('utf-8').strip())
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+    except:
+        return False
+
+while True:
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+    
+    for notified_socket in read_sockets:
+        if notified_socket == s:
+            client_socket, client_address = s.accept()
+            user = receive_message(client_socket)
+            if user is False:
+                continue
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
+            print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
+        else:
+            message = receive_message(notified_socket)
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+            user = clients[notified_socket]
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+            
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
